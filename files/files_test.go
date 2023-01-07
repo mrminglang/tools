4d17446e41ca6fe67a44dadc7d5820155b67f8c6
ming <mrminglang@163.com>
2023-01-07 20:37:26 +0800

    路径

--- files/files_test.go
+++ files/files_test.go
@@ -4,12 +4,14 @@ import (
 	"github.com/mrminglang/tools/dumps"
 	"github.com/mrminglang/tools/files"
 	"github.com/mrminglang/tools/paths"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestIsFileExist(t *testing.T) {
 	path := paths.FullPath("files/files.go")
+	logrus.Infoln("path::", path)
 	exist, err := files.IsFileExist(path)
 	if err != nil {
 		assert.Error(t, err)
