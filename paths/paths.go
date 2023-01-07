5b6bb38a815d5fc7281b8f6ed3347f15042c2450
ming <mrminglang@163.com>
2023-01-07 20:44:14 +0800

    路径

--- paths/paths.go
+++ paths/paths.go
@@ -8,7 +8,7 @@ import (
 // FullPath 获取项目根目录的绝对路径
 func FullPath(path string) string {
 	_, b, _, _ := runtime.Caller(0)
-	basePath := filepath.Join(filepath.Dir(b), "../../../../")
+	basePath := filepath.Join(filepath.Dir(b), "../../../../../")
 	return filepath.Join(basePath, path)
 }
 
