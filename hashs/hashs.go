02d2ed70aef631201131f6d20838352e6c83a923
langming(明朗) <langming@upchina.com>
2023-09-04 15:46:49 +0800

    对字符串进行SHA1签名

--- hashs/hashs.go
+++ hashs/hashs.go
@@ -1,1 +1,26 @@
 package hashs
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+)
+
+// 对字符串进行SHA1签名
+func GenerateSHA1Signature(input string) string {
+	// Convert the input string to bytes
+	data := []byte(input)
+
+	// Create a new SHA1 hash
+	hash := sha1.New()
+
+	// Write the data to the hash
+	hash.Write(data)
+
+	// Get the hashed result
+	signature := hash.Sum(nil)
+
+	// Convert the hashed result to a hexadecimal string
+	signatureHex := hex.EncodeToString(signature)
+
+	return signatureHex
+}
--- hashs/hashs_test.go
+++ hashs/hashs_test.go
@@ -1,1 +1,11 @@
-package hashs
+package hashs_test
+
+import (
+	"github.com/mrminglang/tools/dumps"
+	"github.com/mrminglang/tools/hashs"
+	"testing"
+)
+
+func TestGenerateSHA1Signature(t *testing.T) {
+	dumps.Dump(hashs.GenerateSHA1Signature("111"))
+}
